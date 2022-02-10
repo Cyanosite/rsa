0271262398cae0d4d9b20d0b0365fe51efc32104
Cyanosite <zsomborszenyan@gmail.com>
2022-02-10 14:54:11 +0100

    prime size 512-1024 bits

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -1,57 +1,67 @@
 pub mod algorithms {
-    pub fn euclidean(mut m: i128, mut a: i128) -> i128 {
+    use num_bigint::{BigInt, RandBigInt, Sign, ToBigInt};
+    pub fn euclidean(mut m: BigInt, mut a: BigInt) -> BigInt {
         if a > m {
             let temp = a;
             a = m;
             m = temp;
         }
-        let mut r: i128;
+        let mut r: BigInt;
         loop {
-            r = m.rem_euclid(a);
-            if r == 0 {
+            r = m % a.clone();
+            if r == 0i64.to_bigint().unwrap() {
                 return a;
             }
             m = a;
             a = r;
         }
     }
-    pub fn exponentiation(m: i128, mut a: i128, mut b: i128) -> i128 {
-        let mut c: i128 = 1;
+    pub fn exponentiation(m: BigInt, mut a: BigInt, mut b: BigInt) -> BigInt {
+        let mut c: BigInt = 1i64.to_bigint().unwrap();
         loop {
-            if b & 0x1 == 0x1 {
-                c = (c * a).rem_euclid(m);
+            if b.clone() % 2i64 == 1i64.to_bigint().unwrap() {
+                c = (c * a.clone()) % m.clone();
             }
-            b /= 2;
-            if b == 0 {
-                return if c < 0 { c + m } else { c };
+            b /= 2i64;
+            if b == 0i64.to_bigint().unwrap() {
+                return if c < 0i64.to_bigint().unwrap() {
+                    c + m
+                } else {
+                    c
+                };
             }
-            a = (a * a).rem_euclid(m);
+            a = (a.clone() * a.clone()) % m.clone();
         }
     }
-    pub fn congruence(mut a: i128, mut b: i128, om: i128) -> i128 {
-        let mut m: i128 = om;
-        let mut p: i128 = 0;
+    pub fn congruence(mut a: BigInt, mut b: BigInt, om: BigInt) -> BigInt {
+        let mut m: BigInt = om.clone();
+        let mut p = 0i64.to_bigint().unwrap();
         loop {
-            let t: i128 = m / a;
-            let r = m.rem_euclid(a);
-            if r == 0 {
-                return if b < 0 { om + b } else { b };
+            let t: BigInt = m.clone() / a.clone();
+            let r = m.clone() % a.clone();
+            if r == 0i64.to_bigint().unwrap() {
+                return if b < 0i64.to_bigint().unwrap() {
+                    om.clone() + b
+                } else {
+                    b
+                };
             }
-            let c = (p - t * b).rem_euclid(om);
+            let c = (p - t * b.clone()) % om.clone();
             m = a;
             a = r;
-            p = b;
+            p = b.clone();
             b = c;
         }
     }
-    pub fn prime_check(m: i128) -> bool {
-        use rand::Rng;
+    pub fn prime_check(m: BigInt) -> bool {
         for _k in 0..100 {
-            let a: i128 = rand::thread_rng().gen::<i128>() % (m - 1); //random
-            if euclidean(m, a) != 1 {
+            let low = BigInt::from_bytes_be(Sign::Plus, b"13407807929942597099574024998205846127479365820592393377723561443721764030073546976801874298166903427690031858186486050853753882811946569946433649006084096");
+            let high = m.clone() - 1i64;
+            let a = rand::thread_rng().gen_bigint_range(&low, &high); //random
+            if euclidean(m.clone(), a.clone()) != 1i64.to_bigint().unwrap() {
                 return false;
             }
-            if exponentiation(m, a, m - 1) != 1 {
+            if exponentiation(m.clone(), a.clone(), m.clone() - 1i64) != 1i64.to_bigint().unwrap() {
                 return false;
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -4,59 +4,73 @@ pub use crate::algorithms::algorithms::euclidean;
 pub use crate::algorithms::algorithms::exponentiation;
 pub use crate::algorithms::algorithms::prime_check;
 
-use rand::Rng;
+use num_bigint::{BigInt, RandBigInt, Sign, ToBigInt};
 use std::sync::mpsc;
-const MAGIC_NUMBER: i128 = 65537;
 
 struct Message {
-    message: Vec<i128>,
+    message: Vec<BigInt>,
     is_encrypted: bool,
-    public_key: i128,
-    private_key: (i128, i128),
+    public_key: BigInt,
+    private_key: (BigInt, BigInt),
 }
 
 impl Message {
     fn print(&self) {
         for letter in &self.message {
-            print!("{}", *letter as u8 as char);
+            print!(
+                "{}",
+                *letter.to_signed_bytes_le().first().unwrap() as u8 as char
+            );
         }
         println!("");
     }
     fn encrypt(&mut self) {
         let (tx, rx) = mpsc::channel();
-        let mut primes = [0i128; 2];
+        let mut primes = [
+            BigInt::from_bytes_be(Sign::Plus, b"0"),
+            BigInt::from_bytes_be(Sign::Plus, b"0"),
+        ];
 
+        let low = BigInt::from_bytes_be(Sign::Plus, b"13407807929942597099574024998205846127479365820592393377723561443721764030073546976801874298166903427690031858186486050853753882811946569946433649006084096");
+        let high = BigInt::from_bytes_be(Sign::Plus, b"179769313486231590772930519078902473361797697894230657273430081157732675805500963132708477322407536021120113879871393357658789768814416622492847430639474124377767893424865485276302219601246094119453082952085005768838150682342462881473913110540827237163350510684586298239947245938479716304835356329624224137216");
         std::thread::spawn(move || loop {
-            let prime: i128 = rand::thread_rng().gen::<u64>() as i128 % 1000000000;
-            if prime_check(prime) {
+            let prime = rand::thread_rng().gen_bigint_range(&low, &high);
+            if prime_check(prime.clone()) {
+                println!("{} is a prime", prime);
                 tx.send(prime).unwrap();
                 break;
             }
         });
-
+        let low = BigInt::from_bytes_be(Sign::Plus, b"13407807929942597099574024998205846127479365820592393377723561443721764030073546976801874298166903427690031858186486050853753882811946569946433649006084096");
+        let high = BigInt::from_bytes_be(Sign::Plus, b"179769313486231590772930519078902473361797697894230657273430081157732675805500963132708477322407536021120113879871393357658789768814416622492847430639474124377767893424865485276302219601246094119453082952085005768838150682342462881473913110540827237163350510684586298239947245938479716304835356329624224137216");
         loop {
-            let prime: i128 = rand::thread_rng().gen::<u64>() as i128 % 1000000000;
-            if prime_check(prime) {
+            let prime = rand::thread_rng().gen_bigint_range(&low, &high);
+            if prime_check(prime.clone()) {
+                println!("{} is a prime", prime);
                 primes[1] = prime;
                 break;
             }
         }
         primes[0] = rx.recv().unwrap();
-        self.public_key = primes[0] * primes[1];
-        self.private_key = (primes[0], primes[1]);
+        self.public_key = primes[0].clone() * primes[1].clone();
+        self.private_key = (primes[0].clone(), primes[1].clone());
         for letter in &mut self.message {
-            *letter = exponentiation(self.public_key, *letter, MAGIC_NUMBER);
+            *letter = exponentiation(
+                self.public_key.clone(),
+                letter.clone(),
+                65537i64.to_bigint().unwrap(),
+            );
         }
         self.is_encrypted = true;
     }
     fn decrypt(&mut self) {
         for letter in &mut self.message {
             let decryption_key = congruence(
-                MAGIC_NUMBER,
-                1,
-                (self.private_key.0 - 1) * (self.private_key.1 - 1),
+                65537i64.to_bigint().unwrap(),
+                1i64.to_bigint().unwrap(),
+                (self.private_key.clone().0 - 1i64) * (self.private_key.clone().1 - 1i64),
             );
-            *letter = exponentiation(self.public_key, *letter, decryption_key);
+            *letter = exponentiation(self.public_key.clone(), letter.clone(), decryption_key);
         }
     }
 }
@@ -66,11 +80,13 @@ impl From<&str> for Message {
         let mut mymessage = Message {
             message: Vec::new(),
             is_encrypted: false,
-            public_key: 0,
-            private_key: (0, 0),
+            public_key: 0i64.to_bigint().unwrap(),
+            private_key: (0i64.to_bigint().unwrap(), 0i64.to_bigint().unwrap()),
         };
         for character in temp.chars() {
-            mymessage.message.push(character as i128);
+            mymessage
+                .message
+                .push((character as i64).to_bigint().unwrap());
         }
         mymessage.is_encrypted = false;
         mymessage
@@ -78,7 +94,7 @@ impl From<&str> for Message {
 }
 
 fn main() {
-    let mut message = Message::from("Nobody will know that I'm a catgirl");
+    let mut message = Message::from("Ezt az üzenetet senki sem láthatja!!!");
     message.print();
     message.encrypt();
     message.print();
